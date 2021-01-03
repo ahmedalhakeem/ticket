b6bbb006a4aba20431f0005caca3753934d37666
ahmedalhakeem <ahmkam2011@gmail.com>
2021-01-04 01:20:37 +0300

    created DB

--- ticketapp/forms.py
+++ ticketapp/forms.py
@@ -1,15 +1,21 @@
 from django import forms
+from django.forms import ModelForm
 from ticketapp.models import User, Department, Section, Problems, ProblemType
+
+#class UserForm(ModelForm):
+ #   class meta:
+  #      model = User
+   #     fields = ('first_name', 'last_name', 'email', 'password', 'pc_code', 'deptartment', 'section' )
 
 
 class UserForm(forms.Form):
-    first_name=forms.CharField(title="", required=True,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name',}))
-    last_name = forms.CharField(title="", required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name',}))
-    email = forms.EmailField(title="", required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placrholder':'Email' ,}))
-    password = forms.PasswordInput(title="", required=True, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Password',}))
-    pc-code = forms.CharField(title="", required=False, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your PC code',}))
-    deptartment = forms.ModelChoiceField(queryset=Department.objects.all(), required=True)
-    section = forms.ModelChoiceField(queryset=Section.objects.all(), required=True)
+    first_name=forms.CharField(label="", required=True,widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder': 'First Name',}))
+    last_name = forms.CharField(label="", required=True, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder': 'Last Name',}))
+    email = forms.EmailField(label="", required=True, widget=forms.TextInput(attrs={'class':'form-control form-control-lg', 'placeholder':'Email' ,}))
+    password = forms.PasswordInput(attrs={'class': 'form-control form-control-lg', 'placeholder': 'Password'})
+    pc_code = forms.CharField(label="", required=False, widget=forms.TextInput(attrs={'class':'form-control form-control-lg','placeholder':'enter your PC code',}))
+    deptartment = forms.ModelChoiceField(label="",queryset=Department.objects.all(), required=True, widget=forms.Select(attrs={'class': 'form-control form-control-lg', 'placeholder':'Select Department' }))
+    section = forms.ModelChoiceField(label="",queryset=Section.objects.all(), required=True, widget=forms.Select(attrs={'class': 'form-control form-control-lg', }))
 
 
 
--- ticketapp/views.py
+++ ticketapp/views.py
@@ -7,8 +7,18 @@ def index(request):
     return render(request, "ticketapp/index.html")
 
 def register(request):
+
     
-    new_user = UserForm()
+    new_user = UserForm(request.POST or None)
+    if new_user.is_valid():
+        first_name= new_user.cleaned_data["first_name"]
+        last_name= new_user.cleaned_data["last_name"]
+        email = new_user.cleaned_data["email"]
+        password = new_user.cleaned_data["password"]
+        pc_code = new_user.cleaned_data["pc_code"]
+        department = new_user.cleaned_data["deptartment"]
+        section = new_user.cleaned_data["section"]
+    
     return render(request, "ticketapp/register.html",{
-        "userForm": UserForm()
+        "new_user" : UserForm()
     })
