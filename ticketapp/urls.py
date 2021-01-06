15e8717f075ef291824d6e07fc592b584ae0378f
ahmedalhakeem <ahmkam2011@gmail.com>
2021-01-06 22:24:36 +0300

    fixed auth

--- ticketapp/urls.py
+++ ticketapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register, name = "register"),
-    path("login_page", views.login_page, name="login_page"),
+    path("login_view", views.login_view, name="login_view"),
     path("profile/<int:user_id>", views.profile, name="profile"),
     path("logout_page", views.logout_page, name="logout_page")
 ]
